b46c915e305f9ba1699b83aa734e30be84e50de6
Iskander <knucksie@>
2022-03-17 23:17:20 +0300

    Added incorrect request check

--- third_task/boolean_search.py
+++ third_task/boolean_search.py
@@ -17,17 +17,20 @@ def boolean_search(search_request: str, inverted_index):
     prev_result = set()
     tokens = search_request.split(' ')
     start_token = tokens[0]
-    search_result = inverted_index[start_token]
-    if len(tokens) == 1:
+    try:
+        search_result = inverted_index[start_token]
+        if len(tokens) == 1:
+            return search_result
+        for i in range(1, len(tokens) - 1):
+            if tokens[i] == DISJUNCTION_SIGN:
+                search_result = search_result | inverted_index[tokens[i + 1]]
+            elif tokens[i] == CONJUCTION_SIGN:
+                search_result = search_result & inverted_index[tokens[i + 1]]
+            elif tokens[i] == NEGATION_SIGN:
+                search_result = search_result - inverted_index[tokens[i + 1]]
         return search_result
-    for i in range(1, len(tokens) - 1):
-        if tokens[i] == DISJUNCTION_SIGN:
-            search_result = search_result | inverted_index[tokens[i + 1]]
-        elif tokens[i] == CONJUCTION_SIGN:
-            search_result = search_result & inverted_index[tokens[i + 1]]
-        elif tokens[i] == NEGATION_SIGN:
-            search_result = search_result - inverted_index[tokens[i + 1]]
-    return search_result
+    except KeyError as error:
+        raise ValueError('Nothind found in index')
 
 
 if __name__ == '__main__':
@@ -37,8 +40,11 @@ if __name__ == '__main__':
         print('Error: empty search request.')
         exit(1)
     else:
-        result = boolean_search(search_string, index)
-        if result:
-            print(f'Following documents were found: {", ".join(result)}')
-        else:
-            print('Nothing found.')
+        try:
+            result = boolean_search(search_string, index)
+            if result:
+                print(f'Following documents were found: {", ".join(result)}')
+            else:
+                print('Nothing was found.')
+        except ValueError:
+            print('Nothing was found or search request is incorrect.')
